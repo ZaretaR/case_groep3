e5cdbaf7ca1b50f5fb2a7b19a9b0529f4c2d3566
ZaretaR <152478352+ZaretaR@users.noreply.github.com>
2023-12-08 11:53:35 +0100

    Case_34

--- case/form.js
+++ case/form.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 "use strict"
-=======
-
-
-
->>>>>>> b475390f430bd210598dcc70d3569a06e9afac93
 
 let acc = document.getElementsByClassName("accordon");
 let i;
 
-<<<<<<< HEAD
 function controleerVoorwaardenNaam(){
     if(naamTxt.length < 2 ){
         document.getElementById("naam_error").innerHTML="Minstens 2 karakters lang!";
@@ -27,23 +20,8 @@ function controleerVoorwaardenEmail(){
     }
     else{
         document.getElementById("email_error").innerHTML="";
-=======
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
->>>>>>> b475390f430bd210598dcc70d3569a06e9afac93
     }
-  });
-}
+  };
 
 let voornaamTxt;
 function controleerVoorwaardenVoornaam(){
@@ -56,25 +34,3 @@ function controleerVoorwaardenVoornaam(){
   }
 };
 
-<<<<<<< HEAD
-=======
-function verstuur(){ 
-    voornaamTxt=document.getElementById("voornaam").value;
-
-    allesCorrectIngevuld=true;
-        //als er op de knop geklikt wordt, moet het bijhouden van de fouten gereset worden
-    telSelectedWinToestel=0;
-
-    if(voornaamTxt.length==0){
-      document.getElementById("voornaam_error").innerHTML="Vul in a.u.b.";
-      allesCorrectIngevuld = false;
-    }
-    else{
-      //aparte functie aanmaken om de voorwaarden te controleren
-      controleerVoorwaardenVoornaam()
-    }
-    if(allesCorrectIngevuld){
-      document.write("Naam is correct ingevuld");
-    }
-  };
->>>>>>> b475390f430bd210598dcc70d3569a06e9afac93
